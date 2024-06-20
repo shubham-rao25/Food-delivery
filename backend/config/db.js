b453cd2876736a9c06eb57ce69960caae638c7f6
Shubham Rao <122587669+shubham-rao25@users.noreply.github.com>
2024-06-20 11:33:31 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://shubham:Bettiah2347@cluster0.llbhebs.mongodb.net/food-del"
+      ""
     )
     .then(() => console.log("DB connected"));
 };
